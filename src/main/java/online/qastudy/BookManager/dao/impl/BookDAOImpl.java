b3274d241ae9c88cddb12cc156f8c2664d91bccc
HomeSweet <egormaksimchuk@gmail.com>
2017-10-04 09:57:17 +0300

    BookDAOImpl realisation

--- src/main/java/online/qastudy/BookManager/dao/impl/BookDAOImpl.java
+++ src/main/java/online/qastudy/BookManager/dao/impl/BookDAOImpl.java
@@ -2,23 +2,49 @@ package online.qastudy.BookManager.dao.impl;
 
 import online.qastudy.BookManager.dao.BookDAO;
 import online.qastudy.BookManager.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class BookDAOImpl implements BookDAO {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookDAOImpl.class);
+
+    private SessionFactory sessionFactory;
+
     @Override
     public void addBook(Book book) {
+
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(book);
+        logger.info("Book was saved successfully: " + book.toString());
 
     }
 
     @Override
     public void updateBook(Book book) {
 
+        Session session = this.sessionFactory.getCurrentSession();
+        session.update(book);
+        logger.info("Book was updated successfully: " + book.toString());
+
     }
 
     @Override
     public void removeBook(int id) {
 
+        Session session = this.sessionFactory.getCurrentSession();
+        Book deletedBook = (Book) session.load(Book.class, new Integer(id));
+        if (deletedBook != null) {
+
+            session.delete(deletedBook);
+
+        }
+
+        logger.info("Book was deleted successfully: " + deletedBook.toString());
     }
 
     @Override
