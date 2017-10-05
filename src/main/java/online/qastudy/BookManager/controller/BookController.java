2a3a7aab583393c9741340e307d448afb4031fb3
HomeSweet <egormaksimchuk@gmail.com>
2017-10-05 08:24:34 +0300

    Add code to controller

--- src/main/java/online/qastudy/BookManager/controller/BookController.java
+++ src/main/java/online/qastudy/BookManager/controller/BookController.java
@@ -1,4 +1,19 @@
 package online.qastudy.BookManager.controller;
 
+import online.qastudy.BookManager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class BookController {
+    private BookService bookService;
+
+    @Autowired(required = true)
+    @Qualifier(value = "bookservice")
+    public void setBookService(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    
 }
