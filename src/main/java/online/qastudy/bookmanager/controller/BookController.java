cb2614d33d23995452dd8dd070a1e94069cb4891
HomeSweet <egormaksimchuk@gmail.com>
2017-11-06 22:24:05 +0200

    Build and deploy

--- src/main/java/online/qastudy/bookmanager/controller/BookController.java
+++ src/main/java/online/qastudy/bookmanager/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -30,14 +31,19 @@ public class BookController {
 
     @RequestMapping(value = "books/add", method = RequestMethod.POST)
     public String addBook(@ModelAttribute("book") Book book) {
-
-        if (null == book.getId()) {
+        if (book.getId() == 0) {
             this.bookService.addBook(book);
         } else {
             this.bookService.updateBook(book);
         }
-
         return "redirect:/books";
     }
 
+    @RequestMapping("/remove/{id}")
+    public String removeBook(@PathVariable("id") int id) {
+
+        this.bookService.removeBook(id);
+
+        return "redirect:/books";
+    }
 }
